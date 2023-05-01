79fc3cf672c9758b7dbe1a9f61eb64e4b74a07dc
Zidane Innis <zidaneinnis@gmail.com>
2023-05-01 10:49:30 -0500

    Using select and populate methods

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById("644fd578e4001da1d896e94")
+  User.findById("644fd578e4001da1d896e942")
     .then((user) => {
       req.user = user;
       next();
