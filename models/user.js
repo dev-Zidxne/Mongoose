814295b9c86874cc9d1e3ebc654529f1ca0ec2d3
Zidane Innis <zidaneinnis@gmail.com>
2023-05-01 13:01:10 -0500

    Deleting Cart Items

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const product = require("./product");
 
 const Schema = mongoose.Schema;
 
@@ -24,6 +25,59 @@ const userSchema = new Schema({
     ],
   },
 });
+
+userSchema.methods.addToCart = function (product) {
+  const cartProductIndex = this.cart.items.findIndex((cp) => {
+    return cp.productId.toString() === product._id.toString();
+  });
+
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQuantity,
+    });
+  }
+
+  const updatedCart = {
+    items: updatedCartItems,
+  };
+  this.cart = updatedCart;
+  return this.save();
+};
+
+userSchema.methods.getCart = function (product) {
+  const productIds = this.cart.items.map((item) => {
+    return item.productId;
+  });
+  return product
+    .collection("products")
+    .find({ _id: { $in: productIds } })
+    .toArray()
+    .then((products) => {
+      return products.map((product) => {
+        return {
+          ...product,
+          quantity: this.cart.items.find((item) => {
+            return item.productId.toString() === product._id.toString();
+          }).quantity,
+        };
+      });
+    });
+};
+
+userSchema.methods.removeFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter((item) => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
 
 module.exports = mongoose.model("User", userSchema);
 
