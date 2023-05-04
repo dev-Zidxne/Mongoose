d4e1020df8b94c94e77a24ad31c06f948b9b9f8c
Zidane Innis <zidaneinnis@gmail.com>
2023-05-03 22:22:36 -0500

    Using the Express Session Middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 exports.getLogin = (req, res, next) => {
   //   const isLoggedIn =
   //     req.get("Cookie").split(";")[1].trim().split("=")[1] === "true";
+
+  console.log(req.session.isLoggedIn);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
@@ -9,6 +11,6 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly");
+  req.session.isLoggedIn = true;
   res.redirect("/");
 };
